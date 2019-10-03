725b2dbedcc56b009444ec3084da3286c62c7062
Andre <andregomespin@gmail.com>
2019-10-03 20:55:58 -0300

    tesdts

--- client/board.py
+++ client/board.py
@@ -1,9 +1,21 @@
 class Board:
-    bombMap: {}
-    no_rows_columns: 0
 
-    def print_board(self):
-        return None
+    def ask_number_columns(self):
+        print("Digite número de linhas e colunas", end="\n")
+        return input()
+
+    def ask_pos(self):
+        print("Digite linha", end="\r\n")
+        x = input()
+        print("Digite coluna", end="\r\n")
+        y = input()
+        return str(x) + str(y)
+
+    def print_board(self, no_rows_columns):
+        aux = 0
+        while aux < no_rows_columns:
+            print("A \t" * int(no_rows_columns))
+
 
     def initialize(self):
         return None
